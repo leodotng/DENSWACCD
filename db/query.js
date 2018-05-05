018793f1499672b9a0c53f1e3368267b9bc1c025
leodotng <josh@leo.ng>
2018-05-04 22:49:27 -0600

    added in getby id routes

--- db/query.js
+++ db/query.js
@@ -4,11 +4,21 @@ function getCriticalUpdates(){
     return db('criticalupdates').orderBy('critdetails');
 }
 
+function getCriticalUpdate(id){
+    return db('criticalupdates').where('id', id);
+}
+
 function getShiftPassdowns(){
     return db('shiftpassdown').orderBy('shiftpassdetails')
+}
+function getShiftpassdown(id){
+    return db('shiftpassdown').where('id', id);
 }
 
 module.exports = {
     getCriticalUpdates,
-    getShiftPassdowns
+    getCriticalUpdate,
+    getShiftPassdowns,
+    getShiftpassdown
+
 }
--- routes/index.js
+++ routes/index.js
@@ -16,11 +16,29 @@ router.get('/criticalupdates', function(req, res, next) {
   });
 });
 
+router.get('/criticalupdates/:id', function(req, res, next) {
+  db.getCriticalUpdate(req.params.id)
+  .then(criticalupdates => {
+    res.render('criticalupdates', {
+      criticalupdates: criticalupdates[0]
+    });
+  });
+});
+
 router.get('/shiftpassdown', function(req, res, next) {
   db.getShiftPassdowns()
   .then(shiftpassdown => {
     res.render('shiftpassdown', {
       shiftpassdown: shiftpassdown
+    });
+  });
+});
+
+router.get('/shiftpassdown/:id', function(req, res, next) {
+  db.getShiftPassdown(req.params.id)
+  .then(shiftpassdown => {
+    res.render('shiftpassdown', {
+      shiftpassdown: shiftpassdown[0]
     });
   });
 });
