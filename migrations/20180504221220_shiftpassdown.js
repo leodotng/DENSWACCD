d0f62451b33c6094578d001956185927fe69b8c0
leodotng <josh@leo.ng>
2018-05-04 22:25:24 -0600

    commpleted shiftpassdown migration

--- migrations/20180504221220_shiftpassdown.js
+++ migrations/20180504221220_shiftpassdown.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex, Promise) {
+    return knex.schema.createTable('shiftpassdown', (table) => {
+        table.increments();
+        table.timestamps([useTimestamps], [defaultToNow])
+        table.text('shiftpassdetails');
   
-};
-
-exports.down = function(knex, Promise) {
+    })
+  };
   
-};
+  exports.down = function(knex, Promise) {
+    knex.schema.dropTable('shiftpassdown');
+  };
+  
