73fd59da940107347bb29a4e25d929fd9251d4da
leodotng <josh@leo.ng>
2018-05-05 15:05:35 -0600

    configure for deploy

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-
 require('dotenv').config()
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
+const environment = process.env.NODE_ENV || 'development'
+const config = require('../knexfile.js')[environment]
 
 module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/DENSWACCD'
-    },
+    connection: {
+      localhost: 3000,
+      database: 'denswaccd'
+    }
+  },
 
     production: {
       client: 'pg',
