0592ca57162ab39de06fba19f48295173dd50494
leodotng <josh@leo.ng>
2018-05-04 22:30:48 -0600

    completed shiftpassdown seeds

--- seeds/criticalupdates.js
+++ seeds/criticalupdates.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('criticalupdates').insert([
-        {critdetails: 'rowValue1'},
+        {critdetails: 'No PC Air due to high winds'},
       ]);
     });
 };
