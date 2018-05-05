bdc475504d4e95897cf848498f9c270aec052612
leodotng <josh@leo.ng>
2018-05-05 16:08:11 -0600

    fixed directory for knexfile

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,4 @@
-const config = require('./knexfile')
+const config = require('../knexfile')
 const environment = process.env.NODE_ENV || 'development';
 const connection = config[environment]
 const knex = require('knex')(connection)
